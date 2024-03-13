2c71a564b7873e08d289a7d9b5d46448ea126b24
HenryChinask1 <jeffbaier72@yahoo.com>
2024-03-12 23:01:54 -0400

    main

---
+++ TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+string[] fraudulentOrderIds = new string[3];
+
+fraudulentOrderIds[0] = "A123";
+fraudulentOrderIds[1] = "B345";
+fraudulentOrderIds[2] = "C222";
+//fraudulentOrderIds[3] = "D678";
+*/
+
+string[] fraudulentOrderIds = { "A123", "B345", "C222" };
+
+Console.WriteLine($"First: {fraudulentOrderIds[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIds[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIds[2]}");
+
+fraudulentOrderIds[0] = "F000";
+
+Console.WriteLine($"Reassigned First: {fraudulentOrderIds[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIds.Length} fraudulent cards to process.");
