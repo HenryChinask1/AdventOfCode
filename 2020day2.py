7d1a74235bed58311e655ee64088d00de03398b5
HenryChinask1 <jeffbaier72@yahoo.com>
2024-06-05 15:21:50 -0400

    complete

--- 2020day2.py
+++ 2020day2.py
@@ -1,12 +1,23 @@
 passwords = open('password.txt').read().split('\n')
-password1 = []
+password1 = [] # The test character is i[1[0]], test string is i[2]
+
+okPasswords = 0
 
 for i in passwords:
-    password1.append(i.split())
+    password1.append(i.split(' '))
 
-for j, v in enumerate(password1[0].split('-')):
-    print(j,v)
+minMax = []
+minsMaxs = [] # Two nums, a min and max amount of chars allowed.
+
+for i in password1:
+    minMax.append(i[0])
+
+for i in minMax:
+    minsMaxs.append(i.split('-'))
 
 
-
-#print(password1)
+for idx, password in enumerate(password1):
+        if int(minsMaxs[idx][0]) <= password[2].count(password[1][0]) <= int(minsMaxs[idx][1]):
+            okPasswords += 1
+    
+print(okPasswords)
