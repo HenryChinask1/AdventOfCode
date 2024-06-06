df5ede35029f9c91cbf36d639dda0f03b76d362f
HenryChinask1 <jeffbaier72@yahoo.com>
2024-06-06 16:25:56 -0400

    complete

--- 2022day3.2.py
+++ 2022day3.2.py
@@ -2,29 +2,29 @@ import string
 from itertools import count
 import re
 
-count = 0
+values = 1
 alpha = {}
 ans = 0
 
 s2 = string.ascii_letters
 for i in s2:
-    alpha[i] = count
-    count += 1
+    alpha[i] = values
+    values += 1
 
 rucks = open('sax.txt').read().split('\n')
 
 rucktrios = [rucks[x:x+3] for x in range(0, len(rucks), 3)]
-
-for three in rucktrios:
-    for one in three:
-        for letter in one:
-            print(three)
-            if letter in (three[0], three[1], three[2]):
-                ans += alpha[letter]
-    #match = re.search('/^[A-Za-z]+$/',i[0] + i[1] + i[2])
-    #print(match)
-    #count += alpha[match]
+#print(rucktrios)
 
 
+for rux in rucktrios:
+    #print(rux)
+    for letter in rux[0]:
+        if rux[1].count(letter) >= 1 and rux[2].count(letter) >= 1:
+            ans += alpha[letter] / rux[0].count(letter)
+            #print(ans)
 
-print(ans)
+
+
+
+print(int(round(ans)))
