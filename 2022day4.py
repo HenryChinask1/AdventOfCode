7e3fec161a16e59ea9a294ed314846d1e9c6c451
HenryChinask1 <jeffbaier72@yahoo.com>
2024-06-10 13:48:28 -0400

    Completed

--- 2022day4.py
+++ 2022day4.py
@@ -1,1 +1,24 @@
+cleanLoc = open('sectionIDs.txt').read().replace('-',',').split('\n')
 
+elfLoc = [i.split(',') for i in cleanLoc]
+ans = 0
+ans2 = 0
+
+#print(elfLoc)
+
+# Part 1.
+for idx, group in enumerate(elfLoc):
+    if int(group[0]) >= int(group[2]) and int(group[1]) <= int(group[3]):
+        ans += 1
+    elif int(group[2]) >= int(group[0]) and int(group[3]) <= int(group[1]):
+        ans += 1
+
+# Part 2.
+for idx, group in enumerate(elfLoc):
+    if int(group[0]) <= int(group[3]) and int(group[1]) >= int(group[2]):
+        ans2 += 1
+    elif int(group[2]) <= int(group[1]) and int(group[3]) >= int(group[0]):
+        ans2 += 1
+
+print(f'Part 1: {ans}')
+print(f'Part 2: {ans2}')
