e55569e7878f0b76aef1d5859319a7ad486d0347
HenryChinask1 <jeffbaier72@yahoo.com>
2024-06-15 22:43:56 -0400

    Complete

--- 2021day3.py
+++ 2021day3.py
@@ -1,8 +1,6 @@
-#power = open("2021day3.txt").read().split('\n')
-power = ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010']
+power = open("2021day3.txt").read().split('\n')
+#power = ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010']
 width = len(power[0])
-for x in power:
-    assert len(x) == width
 
 gamma = ''
 epsilon = ''
@@ -11,29 +9,28 @@ for i in range(width):
     
     zeroes = 0
     ones = 0
-    for j in power[i]:
-        if j == '0':
+    for j in range(len(power)):
+        if power[j][i] == '0':
             zeroes += 1
-        elif j == '1':
+        elif power[j][i] == '1':
             ones += 1
+
     if zeroes > ones:
         gamma += '0'
         epsilon += '1'
+
     elif ones > zeroes:
         gamma += '1'
         epsilon += '0'
-
-print(gamma)
-
-print(epsilon)
-
+    
+    elif zeroes == ones:
+        gamma += '1'
+        epsilon += '0'
 
 gammaDecimal = int(gamma, 2)
 epsilonDecimal = int(epsilon, 2)
 
-print(gammaDecimal, epsilonDecimal)
-
-
+#Part one.
 print(gammaDecimal * epsilonDecimal)
 
 
