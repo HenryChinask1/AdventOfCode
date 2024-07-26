6b852621d0ccd838af9f131e3f2f3793f02469b2
HenryChinask1 <jeffbaier72@yahoo.com>
2024-07-26 15:04:15 -0400

    both parts complete

--- 2018day2.py
+++ 2018day2.py
@@ -12,7 +12,7 @@ for box in boxes:
     alphaCount = dict.fromkeys(string.ascii_lowercase, 0)
     for letter in box:
         alphaCount[letter] += 1
-    print(alphaCount)
+    #print(alphaCount)
 
     if 2 in alphaCount.values():
         twos += 1
@@ -21,5 +21,25 @@ for box in boxes:
         threes += 1
         commonBoxes.append(box)
 
+print('Part One:')
 print(twos * threes)
-print(commonBoxes)
+
+def diffLetters(a, b):
+    twoWords = zip(a, b)
+    checker = [0, '', '']
+    for item in twoWords:
+        if item[0] != item[1]:
+            checker[0] += 1
+            checker[1] += '*'
+            checker[2] += '*'
+        else:
+            checker[1] += item[0]
+            checker[2] += item[1]
+    
+    return checker
+
+for a in commonBoxes:
+    for b in commonBoxes:
+        if 1 in diffLetters(a, b):
+            print('Part Two:')
+            print(diffLetters(a, b)[2])
