70ba7d49820f34a3948362cfedb920836b8f5008
HenryChinask1 <jeffbaier72@yahoo.com>
2024-07-26 13:49:42 -0400

    part 1 complete

--- 2018day2.py
+++ 2018day2.py
@@ -1,19 +1,25 @@
 import string
 
-alphaCount = dict.fromkeys(string.ascii_lowercase, 0)
 twos = 0
 threes = 0
+commonBoxes = []
 
 
-boxes = open('boxScans.txt').read().split('\n')
+boxes = open('2018day2.txt').read().split()
+#print(boxes)
 
-for idx, letter in enumerate(boxes):
-    if letter in alphaCount:
+for box in boxes:
+    alphaCount = dict.fromkeys(string.ascii_lowercase, 0)
+    for letter in box:
         alphaCount[letter] += 1
-    options = alphaCount.values()
-    for i in options:
-        if i == 2:
-            twos += 1
-        if i == 3:
-            threes += 1
-    
+    print(alphaCount)
+
+    if 2 in alphaCount.values():
+        twos += 1
+        commonBoxes.append(box)
+    if 3 in alphaCount.values():
+        threes += 1
+        commonBoxes.append(box)
+
+print(twos * threes)
+print(commonBoxes)
