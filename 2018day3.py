b37a2413a6e6cd6ee71c388bffabecdcfe248d49
HenryChinask1 <jeffbaier72@yahoo.com>
2024-07-29 14:03:54 -0400

    part one complete

---
+++ 2018day3.py
@@ -0,0 +1,33 @@
+file = open('2018day3.txt').read().split('\n')
+quilt = [[0] * 1000 for _ in range(1000)] # The empty grid.
+squares = [] # The list of claims.
+ans = 0
+
+for i in file:
+    squares.append(i.split(' '))
+
+def startPos(gridStart):
+    gridStart = gridStart[0:-1].split(',')
+    return((int(gridStart[0]), int(gridStart[1])))
+
+def patchSize(quiltSize):
+    quiltSize = quiltSize.split('x')
+    return((int(quiltSize[0]),int(quiltSize[1])))
+
+
+for id, symbol, gridStart, quiltSize in squares:
+    start = startPos(gridStart)
+    size = patchSize(quiltSize)
+    for col in range(start[1], start[1] + size[1]):
+        for row in range(start[0], start[0] + size[0]):
+            quilt[row][col] += 1
+
+for line in quilt:
+    for inch in line:
+        if inch > 1:
+            ans += 1
+            
+        
+#print(quilt)
+print(ans)
+
