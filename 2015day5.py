4c2ba1620f8b5269309d618d1624ad4949839200
HenryChinask1 <jeffbaier72@yahoo.com>
2024-08-05 14:11:10 -0400

    Part one complete

--- 2015day5.py
+++ 2015day5.py
@@ -1,61 +1,43 @@
-import itertools
+testStrings = open('2015day5.txt').read().split('\n')
 
-testStrings = open('nice_naughty.txt').read().split('\n')
-
-firstTest = []
-secondTest = []
-thirdTest = []
+firstTest = [] # Strings that do not contain 'ab', 'cd', 'pq', or 'xy'.
+secondTest = [] # Strings that contain three total vowels.
+thirdTest = [] # strings that contain a double letter.
 
 
 vowels = ['a', 'e', 'i', 'o', 'u']
 vowelCombos = []
-doubleLetters = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg', 'hh', 'ii', 'kk', 'll', 'mm', 'nn', 'oo', 'pp', 'qq', 'rr', 'ss', 'tt', 'uu', 'vv', 'ww', 'xx', 'yy', 'zz']
+doubleLetters = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg', 'hh', 'ii', 'jj', 'kk', 'll', 'mm', 'nn', 'oo', 'pp', 'qq', 'rr', 'ss', 'tt', 'uu', 'vv', 'ww', 'xx', 'yy', 'zz']
 notAllowed = ['ab', 'cd', 'pq', 'xy']
 niceStrings = 0
-naughtyStrings = 0
+naughtyStrings = len(testStrings)
 
-for i in itertools.product(vowels, repeat=3):
-    
-    vowelCombos.append(i)
 
-print(vowelCombos)
-
+# Run the first check (notAllowed strings).
 for testString in testStrings:
     if notAllowed[0] not in testString and notAllowed[1] not in testString and notAllowed[2] not in testString and notAllowed[3] not in testString:
         firstTest.append(testString)
 
+# Run the second check (three or more vowels).
 for testString in firstTest:
-    for inclusion in doubleLetters:
-        if inclusion in testString:
-            secondTest.append(testString)
+    vowelCount = 0
+    for i in testString:
+        if i in vowels:
+            vowelCount += 1
+    if vowelCount >= 3:
+        secondTest.append(testString)
 
-for word in secondTest:
-    newWord = ''
-    for idx, letter in enumerate(word):
-        if letter in 'aeiou':
-            newWord += letter
-        if idx == 15:
-            thirdTest.append(newWord)
+# Run the third check (double letter).
+for testString in secondTest:
+    for i in range(len(testString) - 1):
+        if testString[i:i + 2] in doubleLetters:
+            thirdTest.append(testString)
 
-for word in thirdTest:
-    if len(word) < 3:
-        continue
-    #if word in 
+thirdTest = set(thirdTest)
 
+niceStrings = len(thirdTest)
 
+naughtyStrings = naughtyStrings - niceStrings
 
-'''
-for inclusion in vowelCombos:
-    for testString in secondTest:
-        if inclusion[0] in testString:
-            newTest = testString.replace(inclusion[0], '', 1)
-        if inclusion[1] in newTest:
-            newTest2 = newTest.replace(inclusion[1], '', 1)
-        if inclusion[2] in newTest2:
-            thirdTest.append(newTest2)
-            niceStrings += 1
-'''
-#print(len(secondTest))
-#print(len(thirdTest))
 print(f'Nice Strings: {niceStrings}')
 print(f'Naughty Strings: {naughtyStrings}')
