33119f7553238b13a77d5bac1a31fbed3644c9c5
HenryChinask1 <jeffbaier72@yahoo.com>
2024-08-06 15:58:44 -0400

    Part one complete

--- 2016day2.py
+++ 2016day2.py
@@ -1,19 +1,15 @@
-movement = open('bathroomcode.txt').read().split('\n')
+movement = open('2016day2.txt').read().split('\n')
 
-grid = [['x', 'x', '1', 'x', 'x'],
-        ['x', '2', '3', '4', 'x'],
-        ['5', '6', '7', '8', '9'],
-        ['x', 'A', 'B', 'C', 'x'],
-        ['x', 'x', 'D', 'x', 'x']]
+grid = [['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7', '8', '9']]
 
-currentDirection = (0, 2) # Start at '5'
+startPos = (1, 1) # Start at '5'
 
-turns = {'U': (0, -1), 'D': (0, 1), 'L': (-1, 0), 'R': (1, 0)}
+turns = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
 
 for moves in movement:
     for move in moves:
-        newCurrentDirection = (max(0, min(currentDirection[0] + turns[move][0], 4)), max(0, min(currentDirection[1] + turns[move][1], 4)))
-        for i in range(50):
-            if [newCurrentDirection[1]][newCurrentDirection[0]] != 'x':
-                currentDirection = newCurrentDirection
-        print(grid[currentDirection[1]][currentDirection[0]])
+        currDir = (max(0, min(startPos[0] + turns[move][0], 2)), max(0, min(startPos[1] + turns[move][1], 2)))
+        startPos = currDir
+    print(grid[currDir[0]][currDir[1]])
