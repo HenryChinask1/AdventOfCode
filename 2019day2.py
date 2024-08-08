54f7bbe234ee8af6edc29f3928e6ca683abfeb46
HenryChinask1 <jeffbaier72@yahoo.com>
2024-08-08 16:29:08 -0400

    Part one complete

---
+++ 2019day2.py
@@ -0,0 +1,13 @@
+opcodes = open('2019day2.txt').read().split(',')
+opcodes[1] = '12'
+opcodes[2] = '2'
+
+for i in range(0, len(opcodes), 4):
+    if opcodes[i] == '99':
+        break
+    elif opcodes[i] == '1':
+        opcodes[int(opcodes[i + 3])] = str(int(opcodes[int(opcodes[i + 1])]) + int(opcodes[int(opcodes[i + 2])]))
+    elif opcodes[i] == '2':
+        opcodes[int(opcodes[i + 3])] = str(int(opcodes[int(opcodes[i + 1])]) * int(opcodes[int(opcodes[i + 2])]))
+
+print(f'Part One: {opcodes[0]}')
