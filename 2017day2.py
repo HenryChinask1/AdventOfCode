56068ea38eaeb0e2c5a0ccff1cb483b992f8a6a9
HenryChinask1 <jeffbaier72@yahoo.com>
2024-08-08 15:04:30 -0400

    Part one and two complete

--- 2017day2.py
+++ 2017day2.py
@@ -1,5 +1,8 @@
-spreadSheet = open('spreadsheet.txt').read().split('\n')
+import itertools
+
+spreadSheet = open('2017day2.txt').read().split('\n')
 dataset = [i.split('\t') for i in spreadSheet]
+print(dataset)
 partOne = 0
 partTwo = 0
 
@@ -7,12 +10,18 @@ partTwo = 0
 for i in range(len(dataset)):
     partOne += max(list(map(int, dataset[i]))) - min(list(map(int, dataset[i])))
 
+def rowCompare(lineIn):
+    total = 0
+    for a, b in itertools.combinations(lineIn, 2):
+        if int(a) % int(b) == 0:
+            total += int(a) / int(b)
+        if int(b) % int(a) == 0:
+            total += int(b) / int(a)
+    return total
 
-for i in range(len(dataset)):
-    for j in range(len(dataset[i])):
-        if int(dataset[i][j]) % int(dataset[j][i]) == 0:
-            partTwo += int(dataset[i][j]) / int(dataset[j][i])
+for line in dataset:
+    partTwo += rowCompare(line)
+
 
 print(partOne)
 print(partTwo)
-
