6a94fd603f535da8ef10978d81a9a60627bf8e4c
HenryChinask1 <jeffbaier72@yahoo.com>
2024-08-09 12:06:35 -0400

    Complete

--- 2022day6.py
+++ 2022day6.py
@@ -1,15 +1,15 @@
-data = open('packetTest.txt').read()
+data = open('Advent of Code Inputs/2022day6.txt').read()
 dataBuffer = [x for x in data]
 
 
 # Part 1 - Packet Markers.
 for i in range(len(dataBuffer) - 4 + 1):
     if len(set(dataBuffer[i: i + 4])) == 4:
-        print(i+4)
+        print(f'Part One: {i+4}')
         break
 
 # Part 2 - Message Markers.
 for i in range(len(dataBuffer) - 14 + 1):
     if len(set(dataBuffer[i: i + 14])) == 14:
-        print(i + 14)
+        print(f'Part Two: {i + 14}')
         break
