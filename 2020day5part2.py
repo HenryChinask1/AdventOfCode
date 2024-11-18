6ac5e7345b16d8db4bfe4b37d5b077e9d43f0338
HenryChinask1 <jeffbaier72@yahoo.com>
2024-11-18 16:24:54 -0500

    day 5 complete

---
+++ 2020day5part2.py
@@ -0,0 +1,41 @@
+p = open("AdventOfCode/Advent of Code Inputs/2020day5.txt").read().split()
+seatId = []
+
+
+def rowCalc(boarder):
+    row = (0, 127)
+    col = (0, 7)
+    finalRow = 0
+    finalCol = 0
+
+    for i in range(6):
+        if boarder[i] == 'F':
+            row = (row[0], ((row[1] - row[0]) // 2) + row[0])
+            #print(row)
+        if boarder[i] == 'B':
+            row = (row[0] + ((row[1] - row[0]) // 2) + 1, row[1])
+            #print(row)
+    finalRow = row[0] if boarder[6] == 'F' else row[1]
+    #print(finalRow)
+
+    for i in range(7, 9):
+        if boarder[i] == 'L':
+            col = (col[0], ((col[1] - col[0]) // 2) + col[0])
+            #print(col)
+        if boarder[i] == 'R':
+            col = (col[0] + ((col[1] - col[0]) // 2) + 1, col[1])
+            #print(col)
+    finalCol = col[0] if boarder[9] == 'L' else col[1]
+    #print(finalCol)
+    newId = finalRow * 8 + finalCol
+    return newId
+
+for boarder in p:
+    seatId.append(rowCalc(boarder))
+
+seatId = sorted(seatId)
+comp = range(seatId[0], seatId[-1] + 1)
+
+for i in comp:
+    if i not in seatId:
+        print(i)
