431db868c361b5ca48eb5fd87935fd70006eba08
HenryChinask1 <jeffbaier72@yahoo.com>
2024-11-22 09:36:54 -0500

    part one complete

---
+++ 2021day6.py
@@ -0,0 +1,18 @@
+
+p = open('AdventOfCode/Advent of Code Inputs/2021day6TEST.txt').read().split(',')
+fish = [int(i) for i in p]
+
+def calcNewFish(fish):
+    for i in range(len(fish)):
+        if fish[i] > 0:
+            fish[i] -= 1
+        elif fish[i] == 0:
+            fish[i] = 6
+            fish.append(8)
+    return fish
+
+for i in range(80):
+    fish = calcNewFish(fish)
+
+
+print(f'Part One = {len(fish)}')
