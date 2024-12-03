964bc8a3cfced1ca6e34f4d323ba1139e9176f56
HenryChinask1 <jeffbaier72@yahoo.com>
2024-12-03 14:36:20 -0500

    Day one complete

--- 2024/2024day1.py
+++ 2024/2024day1.py
@@ -1,18 +1,38 @@
-p = open('AdventOfCode/Advent of Code Inputs/2024day1.txt').read().split('\n')
-leftList = []
-rightList = []
+def partOne():
+    p = open('AdventOfCode/Advent of Code Inputs/2024day1.txt').read().split('\n')
+    leftList = []
+    rightList = []
 
-for i in p:
-    leftList.append(i[0:5])
-    rightList.append(i[-5:])
+    for i in p:
+        leftList.append(i[0:5])
+        rightList.append(i[-5:])
 
-rightList.sort()
-leftList.sort()
-ans = []
+    rightList.sort()
+    leftList.sort()
+    ans = []
 
-i = 0
-while i < len(leftList):
-    ans.append(abs(int(leftList[i]) - int(rightList[i])))
-    i += 1
+    i = 0
+    while i < len(leftList):
+        ans.append(abs(int(leftList[i]) - int(rightList[i])))
+        i += 1
 
-print(f'2024 Day 1 Part 1: {sum(ans)}')
+    print(f'2024 Day 1 Part 1: {sum(ans)}')
+
+def partTwo():
+    p = open('AdventOfCode/Advent of Code Inputs/2024day1.txt').read().split('\n')
+
+    leftList = []
+    rightList = []
+    ans = []
+
+    for i in p:
+        leftList.append(i[0:5])
+        rightList.append(i[-5:])
+
+    for i in leftList:
+        ans.append(int(i) * rightList.count(str(i)))
+
+    print(f'2024 Day 1 Part 2: {sum(ans)}')
+
+partOne()
+partTwo()
