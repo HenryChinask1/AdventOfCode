8b185fa2ce5caa4d47c4f940a8f50684e9feb43f
HenryChinask1 <jeffbaier72@yahoo.com>
2024-12-03 11:47:22 -0500

    part one complete

--- 2024day3.py
+++ 2024day3.py
@@ -1,12 +1,14 @@
 import re
 
 p = open('AdventOfCode/Advent of Code Inputs/2024day3.txt').read()
-puzzle = re.findall(r'mul[(]\d+[,]\d+[)]', p)
-nums = []
+puzzle = re.findall(r'mul[(]\d+[,]\d+[)]', p) # all instructions.
+
+nums = [] # parse values.
 for i in puzzle:
     nums.append(re.findall(r'\d+', i))
-mult = []                
+
+mult = [] # multiply values. 
 for i in nums:
     mult.append(int(i[0]) * int(i[1]))
 
-print(sum(mult))
+print(sum(mult))    
