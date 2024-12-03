55a28d375e4a04b915009ae4c80f8b8e8b677426
HenryChinask1 <jeffbaier72@yahoo.com>
2024-12-03 11:46:18 -0500

    part two complete

--- 2024day3part2.py
+++ 2024day3part2.py
@@ -1,9 +1,9 @@
 import re
 
 p = open('AdventOfCode/Advent of Code Inputs/2024day3.txt').read()
-puzzle = re.findall(r"don[']t[(][)]|do[(][)]|mul[(]\d+[,]\d+[)]", p)
-nums = []
+puzzle = re.findall(r"don[']t[(][)]|do[(][)]|mul[(]\d+[,]\d+[)]", p) # add all instructions.
 
+nums = [] # add valid instructions.
 mulOn = True
 for i in puzzle:
     if i.startswith("don't"):
@@ -13,12 +13,12 @@ for i in puzzle:
     elif i.startswith('mul') and mulOn == True:
         nums.append(i)
 
-mult = []                
+mult = [] # parse instructions.       
 for i in nums:
     mult.append(re.findall(r'\d+', i))
 
-ans = []
+ans = [] # multiply instructions.
 for i in mult:
     ans.append(int(i[0]) * int(i[1]))
 
-print(sum(ans)) 
+print(sum(ans))
