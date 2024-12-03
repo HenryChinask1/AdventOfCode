590c673dc836811cc61ec81f104880bcd75cad50
HenryChinask1 <jeffbaier72@yahoo.com>
2024-12-03 14:41:15 -0500

    Day 3 complete

--- 2024/2024day3.py
+++ 2024/2024day3.py
@@ -1,14 +1,41 @@
 import re
-
 p = open('AdventOfCode/Advent of Code Inputs/2024day3.txt').read()
-puzzle = re.findall(r'mul[(]\d+[,]\d+[)]', p) # all instructions.
 
-nums = [] # parse values.
-for i in puzzle:
-    nums.append(re.findall(r'\d+', i))
+def partOne():
+    puzzle = re.findall(r'mul[(]\d+[,]\d+[)]', p) # all instructions.
 
-mult = [] # multiply values. 
-for i in nums:
-    mult.append(int(i[0]) * int(i[1]))
+    nums = [] # parse values.
+    for i in puzzle:
+        nums.append(re.findall(r'\d+', i))
 
-print(f'2024 Day 3 Part 1: {sum(mult)}')    
+    mult = [] # multiply values. 
+    for i in nums:
+        mult.append(int(i[0]) * int(i[1]))
+
+    print(f'2024 Day 3 Part 1: {sum(mult)}')
+
+def partTwo():
+    puzzle = re.findall(r"don[']t[(][)]|do[(][)]|mul[(]\d+[,]\d+[)]", p) # add all instructions.
+
+    nums = [] # add valid instructions.
+    mulOn = True
+    for i in puzzle:
+        if i.startswith("don't"):
+            mulOn = False
+        elif i.startswith('do()'):
+            mulOn = True
+        elif i.startswith('mul') and mulOn == True:
+            nums.append(i)
+
+    mult = [] # parse instructions.       
+    for i in nums:
+        mult.append(re.findall(r'\d+', i))
+
+    ans = [] # multiply instructions.
+    for i in mult:
+        ans.append(int(i[0]) * int(i[1]))
+
+    print(f'2024 Day 3 Part 2: {sum(ans)}')
+
+partOne()
+partTwo()
