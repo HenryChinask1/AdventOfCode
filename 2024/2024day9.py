025565606189146346866ad2d8844f5cf03b821b
HenryChinask1 <jeffbaier72@yahoo.com>
2024-12-16 12:11:27 -0500

    Part one complete

--- 2024/2024day9.py
+++ 2024/2024day9.py
@@ -23,9 +23,60 @@ def partOne():
             elif val == '.':
                 while fileBlock[-1] == '.':
                     fileBlock.pop()
-                if moved[-1] != fileBlock[-1]:
-                    moved.append(fileBlock[-1])
+                moved.append(fileBlock[-1])
                 fileBlock.pop()
+        moved.pop()
+        return moved
+
+    def updateChecksum(compressedDisk):
+        checkSum = 0
+        for idx, val in enumerate(compressedDisk):
+                checkSum += idx * val
+        return checkSum
+    
+    fileBlock = parseBlocks(puzzle)
+    compressedDisk = moveFiles(fileBlock)
+    ans = updateChecksum(compressedDisk)
+
+    print(f'Part One: {ans}')
+
+def partTwo():
+    def parseBlocks(puzzle):
+        disk = []
+        currDisk = 0
+        for idx, num in enumerate(puzzle):
+            if idx % 2 == 0:
+                for _ in range(num):
+                    disk.append(currDisk)
+                currDisk += 1
+            elif idx % 2 != 0:
+                for _ in range(num):
+                    disk.append('.')
+        return disk
+    
+    def moveFiles(fileBlock):
+        moved = []
+        for idx, val in enumerate(fileBlock):
+            if val != '.':
+                moved.append(val)
+            elif val == '.':
+                firstDot = idx
+                lastDot = 0
+                for i in fileBlock[idx:]:
+                    if i != '.':
+                        lastDot = fileBlock.index(i) - firstDot
+                for j in range(lastDot):
+                    prevNum = fileBlock[-1]
+                    indices = []
+                    for num in fileBlock[::-1]:
+                        if num == prevNum and num != '.':
+                            indices.append(num)
+                        if num != prevNum or num == '.':
+                            indices = []
+                            break
+                    if indices:
+                        for i in indices:
+                            moved.append(i)
         return moved
     
     def updateChecksum(compressedDisk):
@@ -38,15 +89,7 @@ def partOne():
     compressedDisk = moveFiles(fileBlock)
     ans = updateChecksum(compressedDisk)
 
-    print(f'Part One: {ans}')
-    print(f'6415449222092 is not the answer')
-    print(f'6415449267161 is not the answer')
-    print(f'6418726729910 is not the answer')
-    print(f'2648643289022 is not the answer')
-
-
-def partTwo():
-    pass
+    print(f'Part Two: {ans}')
 
 partOne()
 partTwo()
