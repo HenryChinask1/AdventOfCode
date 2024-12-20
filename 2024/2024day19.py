1e9818ccc849a15695105a33c698b15d4babe8e0
HenryChinask1 <jeffbaier72@yahoo.com>
2024-12-20 16:25:29 -0500

    incomplete

---
+++ 2024/2024day19.py
@@ -0,0 +1,11 @@
+import itertools
+
+p = open('AdventOfCode/Advent of Code Inputs/2024day19TEST.txt').read().split('\n\n')
+towelPatterns = [i for i in p[0].split('.')]
+towelsToMake = [i for i in p[1].split('\n')]
+
+print(towelPatterns)
+print(towelsToMake)
+towels = 0
+
+
