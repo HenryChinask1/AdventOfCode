3fad95d9a32dbb218b9daa21d6ff3b098d32e961
HenryChinask1 <jeffbaier72@yahoo.com>
2024-12-19 21:09:57 -0500

    complete

--- 2024/2024day18.py
+++ 2024/2024day18.py
@@ -37,7 +37,33 @@ def partOne():
                 queue.append(path + [(nx, ny)])
 
 def partTwo():
-    pass
+
+    def djikstra(grid):
+        start = (0, 0)
+        end = (gridSize - 1, gridSize - 1)
+        queue = deque([[start]])
+        visited = set([start])
+
+        while queue:
+            path = queue.popleft()
+            x, y = path[-1]
+            
+            for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] != '#' and (nx, ny) not in visited:
+                    visited.add((nx, ny))
+                    queue.append(path + [(nx, ny)])
+        if end not in visited:
+            return True
+        return False
+
+    gridSize = 71
+    grid = [['.' for i in range(gridSize)] for i in range(gridSize)]
+    for idx, coord in enumerate(puzzle):
+        grid[coord[1]][coord[0]] = '#'
+        if djikstra(grid):
+            print(f'Part Two: {coord[0], coord[1]}')
+            break
 
 partOne()
 partTwo()
