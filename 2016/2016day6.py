8c70a168bfeff86781e7f8df2acac28f2d05544f
HenryChinask1 <jeffbaier72@yahoo.com>
2025-01-02 13:10:25 -0500

    complete

---
+++ 2016/2016day6.py
@@ -0,0 +1,46 @@
+p = open('AdventOfCode/Advent of Code Inputs/2016day6.txt').read().split('\n')
+
+def partOne():
+
+    def counter(line):
+        count = {i:0 for i in 'abcdefghijklmnopqrstuvwxyz'}
+        for i in line:
+            count[i] += 1
+        return max(count, key=count.get)
+    
+    puzzle = []
+    for i in range(len(p[0])):
+        col = []
+        for line in p:
+            col.append(line[i])
+        puzzle.append(col)
+    message = []
+    for i in puzzle:
+        message.append(counter(i))
+
+    
+    print(f'Part One: {''.join(message)}')
+
+def partTwo():
+    
+    def counter(line):
+        count = {i:0 for i in 'abcdefghijklmnopqrstuvwxyz'}
+        for i in line:
+            count[i] += 1
+        return min(count, key=count.get)
+    
+    puzzle = []
+    for i in range(len(p[0])):
+        col = []
+        for line in p:
+            col.append(line[i])
+        puzzle.append(col)
+    message = []
+    for i in puzzle:
+        message.append(counter(i))
+
+    
+    print(f'Part Two: {''.join(message)}')
+
+partOne()
+partTwo()
