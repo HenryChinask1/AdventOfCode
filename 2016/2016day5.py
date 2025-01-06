9023b559da7ee7a2872447bc5b1fde4f7f65d0be
HenryChinask1 <jeffbaier72@yahoo.com>
2025-01-05 22:15:39 -0500

    incomplete

---
+++ 2016/2016day5.py
@@ -0,0 +1,13 @@
+import hashlib
+
+p = open('AdventOfCode/Advent of Code Inputs/2016day5TEST.txt').read()
+
+def partOne():
+    ans = hashlib.md5(b'abc')
+    print(f'Part One: {ans.digest()}')
+
+def partTwo():
+    pass
+
+partOne()
+partTwo()
