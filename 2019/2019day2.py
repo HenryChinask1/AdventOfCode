72709dc7a4197b632925bea5f83aec6eebbad8e2
HenryChinask1 <jeffbaier72@yahoo.com>
2025-01-07 17:08:48 -0500

    complete

--- 2019/2019day2.py
+++ 2019/2019day2.py
@@ -1,24 +1,46 @@
-opcodes = open('AdventOfCode/Advent of Code Inputs/2019day2.txt').read().split(',')
+def partOne():
+    opcodes = [int(i) for i in open('AdventOfCode/Advent of Code Inputs/2019day2.txt').read().split(',')]
+    opcodes[1], opcodes[2] = 12, 2
 
-def checkOps(opcodes, codeZero, codeOne):
-    opcodes[1] = codeZero
-    opcodes[2] = codeOne
-    newList = [None] * len(opcodes)
-    for i in range(0, len(opcodes), 4):
-        if opcodes[i] == '99':
-            return newList[0:3]
-        elif opcodes[i] == '1':
-            opcodes[int(opcodes[i + 3])] = str(int(opcodes[int(opcodes[i + 1])]) + int(opcodes[int(opcodes[i + 2])]))
-        elif opcodes[i] == '2':
-            opcodes[int(opcodes[i + 3])] = str(int(opcodes[int(opcodes[i + 1])]) * int(opcodes[int(opcodes[i + 2])]))
+    def opcodeOne(opcode, i):
+        opcode[opcode[i + 3]] = opcode[opcode[i + 1]] + opcode[opcode[i + 2]]
+        return opcode
 
-print(f'Part One: {checkOps(opcodes, 12, 2)[0]}')
+    def opcodeTwo(opcode, i):
+        opcode[opcode[i + 3]] = opcode[opcode[i + 1]] * opcode[opcode[i + 2]]
+        return opcode
 
-for i in range(100):
-    for j in range(100):
-      if checkOps(opcodes, i, j)[0] == 19690720:
-          noun = checkOps(opcodes, i, j)[1]
-          verb = checkOps(opcodes, i, j)[2]
-          print(f'Part Two: {100 * noun + verb}')
-          break
+    for i in range(0, len(opcodes) - 4, 4):
+        if opcodes[i] == 99:
+            print(f'Part One: {opcodes[0]}')
+        if opcodes[i] == 1:
+            opcodes = opcodeOne(opcodes, i)
+        if opcodes[i] == 2:
+            opcodes = opcodeTwo(opcodes, i)
 
+
+def partTwo(noun, verb):
+    opcodes = [int(i) for i in open('AdventOfCode/Advent of Code Inputs/2019day2.txt').read().split(',')]
+    opcodes[1], opcodes[2] = noun, verb
+
+    def opcodeOne(opcode, i):
+        opcode[opcode[i + 3]] = opcode[opcode[i + 1]] + opcode[opcode[i + 2]]
+        return opcode
+
+    def opcodeTwo(opcode, i):
+        opcode[opcode[i + 3]] = opcode[opcode[i + 1]] * opcode[opcode[i + 2]]
+        return opcode
+
+    for i in range(0, len(opcodes) - 4, 4):
+        if opcodes[i] == 99:
+            if opcodes[0] == 19690720:
+                print(f'Part One: {(100 * noun) + verb}')
+        if opcodes[i] == 1:
+            opcodes = opcodeOne(opcodes, i)
+        if opcodes[i] == 2:
+            opcodes = opcodeTwo(opcodes, i)
+
+partOne()
+for noun in range(1, 100):
+    for verb in range(1, 100):
+        partTwo(noun, verb)
