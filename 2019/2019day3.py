31e825d615148dbf739f33e0a2da3eaf892d65e2
HenryChinask1 <jeffbaier72@yahoo.com>
2025-01-09 14:50:43 -0500

    part one complete

--- 2019/2019day3.py
+++ 2019/2019day3.py
@@ -1,16 +1,134 @@
-grid = [[0 for i in range(10000)] for j in range(10000)]
-moves = open('Advent of Code Inputs/2019day3.txt').read().split('\n')
-start = grid[0][0]
-distances = []
+# Wire Crossings
 
-for line in moves:
-    for move in line:
-        grid[line][move] += 1
+def partOne():
 
-for col in grid:
-    for row in col:
-        if grid[row][col] > 1:
-            distances.append((abs(row - 500) + abs(col - 500)))
+    def parseInput():
+        p = open('AdventOfCode/Advent of Code Inputs/2019day3.txt').read().split('\n')
+        puzzle = []
+        for i in p:
+            puzzle.append([i.split(',')])
+        wireOne = []
+        wireTwo = []
+
+        for wire in puzzle[0][0]:
+            wireOne.append([wire[0], wire[1:]])
+        for wire in puzzle[1][0]:
+            wireTwo.append([wire[0], wire[1:]])
+        return wireOne, wireTwo
+    
+    def addWire(piece, points):
+        currPos = [0, 0]
+        for i in piece:
+            currDir = i[0]
+            if currDir == 'L':
+                end = currPos[1] - int(i[1])
+                while currPos[1] > end:
+                    currPos = [currPos[0], currPos[1] - 1]
+                    if (currPos[0], currPos[1]) not in points:
+                        points[currPos[0], currPos[1]] = 1
+                    else:
+                        points[currPos[0], currPos[1]] += 1
+            elif currDir == 'R':
+                end = currPos[1] + int(i[1])
+                while currPos[1] < end:
+                    currPos = [currPos[0], currPos[1] + 1]
+                    if (currPos[0], currPos[1]) not in points:
+                        points[currPos[0], currPos[1]] = 1
+                    else:
+                        points[currPos[0], currPos[1]] += 1
+            elif currDir == 'U':
+                end = currPos[0] - int(i[1])
+                while currPos[0] > end:
+                    currPos = [currPos[0] - 1, currPos[1]]
+                    if (currPos[0], currPos[1]) not in points:
+                        points[currPos[0], currPos[1]] = 1
+                    else:
+                        points[currPos[0], currPos[1]] += 1
+            elif currDir == 'D':
+                end = currPos[0] + int(i[1])
+                while currPos[0] < end:
+                    currPos = [currPos[0] + 1, currPos[1]]
+                    if (currPos[0], currPos[1]) not in points:
+                        points[currPos[0], currPos[1]] = 1
+                    else:
+                        points[currPos[0], currPos[1]] += 1
+        return points
+    
+    first, second = parseInput()
+    gridOne = {}
+    gridTwo = {}
+    gridOne = addWire(first, gridOne)
+    gridTwo = addWire(second, gridTwo)
+    crosses = list(filter(lambda x: x in gridOne, gridTwo))
+    
+    ans = []
+
+    for i in crosses:
+        ans.append(abs(0 - i[0]) + abs(0 - i[1]))
+
+    print(f'Part One: {min(ans)}')
+
+def partTwo():
+
+    def parseInput():
+        p = open('AdventOfCode/Advent of Code Inputs/2019day3TEST1.txt').read().split('\n')
+        puzzle = []
+        for i in p:
+            puzzle.append([i.split(',')])
+        wireOne = []
+        wireTwo = []
+
+        for wire in puzzle[0][0]:
+            wireOne.append([wire[0], wire[1:]])
+        for wire in puzzle[1][0]:
+            wireTwo.append([wire[0], wire[1:]])
+        return wireOne, wireTwo
+    
+    def addWire(piece):
+        points = []
+        currPos = [0, 0]
+        for i in piece:
+            currDir = i[0]
+            if currDir == 'L':
+                end = currPos[1] - int(i[1])
+                while currPos[1] > end:
+                    currPos = [currPos[0], currPos[1] - 1]
+                    points.append([currPos[0], currPos[1], i[1]])
+            elif currDir == 'R':
+                end = currPos[1] + int(i[1])
+                while currPos[1] < end:
+                    currPos = [currPos[0], currPos[1] + 1]
+                    points.append([currPos[0], currPos[1], i[1]])
+            elif currDir == 'U':
+                end = currPos[0] - int(i[1])
+                while currPos[0] > end:
+                    currPos = [currPos[0] - 1, currPos[1]]
+                    points.append([currPos[0], currPos[1], i[1]])
+            elif currDir == 'D':
+                end = currPos[0] + int(i[1])
+                while currPos[0] < end:
+                    currPos = [currPos[0] + 1, currPos[1]]
+                    points.append([currPos[0], currPos[1], i[1]])
+        return points
+    
+    first, second = parseInput()
+    gridOne = addWire(first)
+    gridTwo = addWire(second)
+    crosses = list(filter(lambda x: x in gridOne, gridTwo))
+
+    res = 0
+
+    for i in gridOne:
+        res += 1
+        if i in crosses:
+            break
+    for i in gridTwo:
+        res += 1
+        if i in crosses:
+            break
+
+    print(f'Part Two: {res}')
 
 
-print(max(distances))
+partOne()
+partTwo()
