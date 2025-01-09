9c383641db8683832e59c08c4a8a1693f380bace
HenryChinask1 <jeffbaier72@yahoo.com>
2025-01-09 16:42:06 -0500

    complete

--- 2019/2019day4.py
+++ 2019/2019day4.py
@@ -1,18 +1,44 @@
+# Password Check
+
 p = 246515, 739105
-valids = 0
+validsPartOne = 0
+validsPartTwo = 0
 
 def incCheck(num):
-    for i in range(5):
+    for i in range(len(num) - 1):
         if int(num[i]) > int(num[i + 1]):
             return False
     return True
+
+def doubleCheck(num):
+    count = 1
+    currNum = num[0]
+    for i in range(1, len(num)):
+        if num[i] == currNum:
+            count += 1
+        else:
+            if count == 2:
+                return True
+            else:
+                currNum = num[i]
+                count = 1
+    if count == 2:
+        return True
+    return False
 
 for i in range(p[0], p[1] + 1):
     i = str(i)
     for j in range(5):
         if i[j] == i[j + 1]:
             if incCheck(i):
-                valids += 1
+                validsPartOne += 1
                 break
-            
-print(valids)
+
+for i in range(p[0], p[1] + 1):
+    if doubleCheck(str(i)) and incCheck(str(i)):
+        validsPartTwo += 1
+
+print(f'Part One: {validsPartOne}')
+print(f'Part Two: {validsPartTwo}')
+
+
