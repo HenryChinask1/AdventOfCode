ec1bbbb5e7a4f437e4a2010552eed19fadb71f9f
HenryChinask1 <jeffbaier72@yahoo.com>
2025-01-16 16:00:34 -0500

    part one complete

---
+++ 2023/2023day4.py
@@ -0,0 +1,32 @@
+p = open('AdventOfCode/Advent of Code Inputs/2023day4.txt').read().split('\n')
+cards = []
+i = 1
+for card in p:
+    cards.append([i, card[card.index(':') + 2:card.index('|') - 1], card[card.index('|') + 2:]])
+    i += 1
+puzzle = []
+
+for i in cards:
+    adder = [i[0]]
+    adder.append(i[1].split())
+    adder.append(i[2].split())
+    puzzle.append(adder)
+
+
+def partOne():
+    ans = 0
+    for i in puzzle:
+        match = 0
+        for num in i[1]:
+            if num in i[2]:
+                match += 1
+        if match > 0:
+            ans += (2 ** (match - 1))
+
+    print(f'Part One: {ans}')
+
+def partTwo():
+    pass
+
+partOne()
+partTwo()
