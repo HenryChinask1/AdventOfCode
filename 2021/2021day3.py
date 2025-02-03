181fa2e53731a181cecea15272b3a5f676d9bb6d
HenryChinask1 <jeffbaier72@yahoo.com>
2025-02-03 15:46:48 -0500

    2021day3 update

--- 2021/2021day3.py
+++ 2021/2021day3.py
@@ -1,36 +1,46 @@
-power = open("Advent of Code Inputs/2021day3.txt").read().split('\n')
-#power = ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010']
-width = len(power[0])
+power = open("AdventOfCode/Advent of Code Inputs/2021day3TEST.txt").read().split('\n')
 
-gamma = ''
-epsilon = ''
+def partOne():
+    gamma = ''
+    epsilon = ''
+    for i in range(len(power[0])):
+        zeroes = 0
+        ones = 0
+        for j in range(len(power)):
+            if power[j][i] == '0':
+                zeroes += 1
+            elif power[j][i] == '1':
+                ones += 1
 
-for i in range(width):
-    
-    zeroes = 0
-    ones = 0
-    for j in range(len(power)):
-        if power[j][i] == '0':
-            zeroes += 1
-        elif power[j][i] == '1':
-            ones += 1
+        if zeroes > ones:
+            gamma += '0'
+            epsilon += '1'
 
-    if zeroes > ones:
-        gamma += '0'
-        epsilon += '1'
+        elif ones >= zeroes:
+            gamma += '1'
+            epsilon += '0'
 
-    elif ones > zeroes:
-        gamma += '1'
-        epsilon += '0'
-    
-    elif zeroes == ones:
-        gamma += '1'
-        epsilon += '0'
+    gammaDecimal = int(gamma, 2)
+    epsilonDecimal = int(epsilon, 2)
 
-gammaDecimal = int(gamma, 2)
-epsilonDecimal = int(epsilon, 2)
+    print(f'Part One: {gammaDecimal * epsilonDecimal}')
+    return gamma
 
-#Part one.
-print(gammaDecimal * epsilonDecimal)
+def partTwo():
+    oxRate = []
+    for i in range(len(power[0])):
+        zeroes = 0
+        ones = 0
+        for j in range(len(power)):
+            if power[j][i] == '0':
+                zeroes += 1
+            else:
+                ones += 1
+        if zeroes > ones:
+            for j in range(len(power)):
+                if power[j][i] == '0':
+                    oxRate.append(power[j])
+    print(oxRate)
 
-
+partOne()
+partTwo()
