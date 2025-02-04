a4091c7ab486feb99308be1be36abe60e6049470
HenryChinask1 <jeffbaier72@yahoo.com>
2025-02-04 15:39:33 -0500

    Refactor, both parts complete

--- 2015/2015day2.py
+++ 2015/2015day2.py
@@ -1,18 +1,29 @@
-with open('Advent of Code Inputs/2015day2.txt') as f:
+with open('AdventOfCode/Advent of Code Inputs/2015day2.txt') as f:
     boxDims = list(item.split('x') for item in f.read().split('\n'))
 
-total = 0
-total2 = 0
+def partOne():
+    total = 0
 
+    for box in boxDims:
+        l = int(box[0])
+        w = int(box[1])
+        h = int(box[2])
+        total += (l * w * 2) + (w * h * 2) + (l * h * 2)
+        total += min((l*w), (w*h), (l*h))
+    
+    print(f'Part One: {total}')
 
-for box in boxDims:
-    l = int(box[0])
-    w = int(box[1])
-    h = int(box[2])
-    total += (l * w * 2) + (w * h * 2) + (l * h * 2)
-    total += min((l*w), (w*h), (l*h))
-    total2 += min((l * 2 + w * 2), (w * 2 + h * 2), (l * 2 + h * 2))
-    total2 += l * w * h
+def partTwo():
+    total = 0
 
-print(f'Part One: {total}')
-print(f'Part Two: {total2}')
+    for box in boxDims:
+        l = int(box[0])
+        w = int(box[1])
+        h = int(box[2])
+        total += min((l * 2 + w * 2), (w * 2 + h * 2), (l * 2 + h * 2))
+        total += l * w * h
+
+    print(f'Part Two: {total}')
+
+partOne()
+partTwo()
