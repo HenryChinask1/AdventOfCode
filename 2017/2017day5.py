452fd8966a866841cb007cdc9b32d7377a276492
HenryChinask1 <jeffbaier72@yahoo.com>
2025-02-19 13:34:18 -0500

    both parts complete

--- 2017/2017day5.py
+++ 2017/2017day5.py
@@ -1,8 +1,7 @@
-f = open('AdventOfCode/Advent of Code Inputs/2017day5TEST.txt').read().split()
-jumps = [int(i) for i in f]
+f = open('AdventOfCode/Advent of Code Inputs/2017day5.txt').read().split()
 
 def partOne():
-
+    jumps = [int(i) for i in f]
     i = 0
     steps = 0
     while i < len(jumps):
@@ -14,17 +13,17 @@ def partOne():
     print(f'Part One: {steps}')
 
 def partTwo():
-    
+    jumps = [int(i) for i in f]
     i = 0
     steps = 0
     while i < len(jumps):
         moveTo = jumps[i]
-        jumps[i] += 1
         if moveTo >= 3:
-            i += 1
-        else:
-            i -= 1
-        steps += 1    
+            jumps[i] -= 1
+        elif moveTo < 3:
+            jumps[i] += 1
+        i += moveTo
+        steps += 1
         
     print(f'Part Two: {steps}')
 
