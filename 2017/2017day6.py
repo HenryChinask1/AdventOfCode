5c88229685767ddae4d602ba86c1cb87cc7260c1
HenryChinask1 <jeffbaier72@yahoo.com>
2025-02-20 16:36:58 -0500

    both parts complete

--- 2017/2017day6.py
+++ 2017/2017day6.py
@@ -20,8 +20,25 @@ def partOne(steps, ans, puzzle):
             ans.append(compare)
     return None
 
-def partTwo():
-    pass
+def partTwo(steps, ans, puzzle):
+    for _ in range(10000):
+        moveMemory = max(puzzle)
+        idx = puzzle.index(max(puzzle))
+        puzzle[idx] = 0
+        while moveMemory:
+            if idx < len(puzzle) - 1:
+                idx += 1
+            else:
+                idx = 0
+            puzzle[idx] += 1
+            moveMemory -= 1
+        steps += 1
+        compare = tuple(puzzle)
+        if len(ans) != len(set(ans)):
+            return len(ans) - ans.index(compare)
+        else:
+            ans.append(compare)
+    return None
 
 print(f'Part One: {partOne(steps=0, ans=[], puzzle=[int(i) for i in p.split()])}')
-partTwo()
+print(f'Part Two: {partTwo(steps=0, ans=[], puzzle=[int(i) for i in p.split()])}')
