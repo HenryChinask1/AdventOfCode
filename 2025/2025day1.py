154d202c7175ebda0f6c6be696a973daed58c2c6
HenryChinask1 <jeffbaier72@yahoo.com>
2025-12-01 00:33:58 -0500

    2025 day 1 solved

--- 2025/2025day1.py
+++ 2025/2025day1.py
@@ -1,10 +1,69 @@
 def partOne():
     data = open('Advent of Code Inputs/2025day1.txt').read().split('\n')
+    turns = [(i[0], int(i[1:])) for i in data]
+    res = 0
+
+    currDial = 50
+
+    def countUp(currDial, turn):
+        while turn:
+            currDial += 1
+            turn -= 1
+            if currDial > 99:
+                currDial = 0
+        return currDial
     
-    pass
+    def countDown(currDial, turn):
+        while turn:
+            currDial -= 1
+            turn -= 1
+            if currDial < 0:
+                currDial = 99
+        return currDial
+
+    for turn in turns:
+        if currDial == 0:
+            res += 1
+        if turn[0] == 'L':
+            currDial = countDown(currDial, turn[1])
+        else:
+            currDial = countUp(currDial, turn[1])
+    print(res)
 
 def partTwo():
-    pass
+
+    data = open('Advent of Code Inputs/2025day1.txt').read().split('\n')
+    turns = [(i[0], int(i[1:])) for i in data]
+    currDial = [0, 50]
+
+    def countUp(currDial, turn, res):
+        while turn:
+            currDial += 1
+            turn -= 1
+            if currDial == 0:
+                res += 1
+            if currDial > 99:
+                currDial = 0
+                res += 1
+        return [res, currDial]
+    
+    def countDown(currDial, turn, res):
+        while turn:
+            currDial -= 1
+            turn -= 1
+            if currDial == 0:
+                res += 1
+            if currDial < 0:
+                currDial = 99
+        return [res, currDial]
+
+    for turn in turns:
+        if turn[0] == 'L':
+            currDial = countDown(currDial[1], turn[1], currDial[0])
+        else:
+            currDial = countUp(currDial[1], turn[1], currDial[0])
+        
+    print(currDial[0])
 
 partOne()
 partTwo()
