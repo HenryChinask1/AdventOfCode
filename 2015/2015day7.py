ce78c02777b1bf54488366d64bed750ee5e61962
HenryChinask1 <jeffbaier72@yahoo.com>
2026-01-11 00:03:55 -0500

    working on day 7 solution.

--- 2015/2015day7.py
+++ 2015/2015day7.py
@@ -1,3 +1,6 @@
+import numpy as np
+from collections import defaultdict
+
 input = open('Advent of Code Inputs/2015day7TEST.txt').read().split('\n')
 
 circuits = []
@@ -16,6 +19,56 @@ def Sort(circuits):
     
     return circuits
 
+# puzzleInput = circuits
 puzzleInput = Sort(circuits)
 circuits.sort(key=len)
-print(puzzleInput)
+
+ans = defaultdict()
+
+def checkVals(parts, op):
+    if op == 'NOT':
+        return ['NOT', parts[1], parts[2]] # Both are letters.
+    
+    elif op == 'ASSIGN':
+        part1 = np.uint16(parts[0])
+        part2 = parts[2]
+        return [part1, part2]
+
+    else:
+        if parts[0].isdigit():
+            part1 = np.uint16(int(parts[0]))
+        else:
+            part1 = parts[0]
+        if parts[2].isdigit():
+            part2 = np.uint16(int(parts[2]))
+        else:
+            part2 = parts[1]
+        if parts[4].isdigit():
+            part3 = np.uint16(int(parts[4]))
+        else:
+            part3 = parts[4]
+        return [op, part1, part2, part3]
+
+
+for i in range(len(puzzleInput)):
+    if 'NOT' in puzzleInput[i]:
+        parts = checkVals(puzzleInput[i], 'NOT')
+        ans[parts[0]] = ~ans[parts[1]]
+    elif 'AND' in puzzleInput[i]:
+        parts = checkVals(puzzleInput[i], 'AND')
+        ans[parts[4]] = ans[parts[2]] & ans[parts[3]]
+    elif 'OR' in puzzleInput[i]:
+        parts = checkVals(puzzleInput[i], 'OR')
+        ans[parts[4]] = ans[parts[2]] | ans[parts[3]]
+    elif 'LSHIFT' in puzzleInput[i]:
+        parts = checkVals(puzzleInput[i], 'LSHIFT')
+        ans[parts[4]] = ans[parts[2]] << ans[parts[3]]
+    elif 'RSHIFT' in puzzleInput[i]:
+        parts = checkVals(puzzleInput[i], 'RSHIFT')
+        ans[parts[4]] = ans[parts[2]] >> ans[parts[3]]
+    else:
+        parts = checkVals(puzzleInput[i], 'ASSIGN')
+        ans[parts[1]] = parts[0]
+
+print(ans)
+print(f'Part One: {ans['a']}')
