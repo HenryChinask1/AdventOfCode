9a9f6c17eb1c4da18b54cf318e8568fca30622bf
henryChinask1 <jeffbaier72@yahoo.com>
2026-01-15 15:58:40 -0500

    2016 day 7 part one complete

--- 2016/2016day7.py
+++ 2016/2016day7.py
@@ -4,31 +4,42 @@ p = open('Advent of Code Inputs/2016day7.txt').read().split('\n')
 puzzle = []
 
 for line in p:
-    i = re.split(r'\[|\]', line)
-    puzzle.append(i)
-print(puzzle)
+    start = 0
+    puzLine = []
+    for i in range(len(line)):
+        if line[i] == '[':
+            puzLine.append(['in', line[start:i]])
+            start = i + 1
+        elif line[i] == ']':
+            puzLine.append(['out', line[start:i]])
+            start = i + 1
+        elif i == len(line) - 1:
+            puzLine.append(['in', line[start:]])
+            start = 0
+            puzzle.append(puzLine)
+            puzLine = []
 
 def partOne():
 
     ans = 0
     
-    def checkAbba(phrase, supportsIfYes=True):
+    def checkAbba(phrase):
         for i in range(0, len(phrase) - 3):
             if phrase[i] == phrase[i + 3] and phrase[i + 1] == phrase[i + 2] and phrase[i] != phrase[i + 1]:
-                return supportsIfYes
-        return not supportsIfYes
+                return True
+        return False
 
     for line in puzzle:
-        for idx, phrase in enumerate(line):
-            if idx % 2 == 0:
-                support = True
-            else:
-                support = False
-            if checkAbba(phrase, support) == True:
-                support = False
-                if checkAbba(phrase) == True or checkAbba(line[2]) == True:
-                    ans += 1
-                    support = True
+        supports = False
+        for piece in line:
+            if piece[0] == 'in':
+                supports = max(supports, checkAbba(piece[1]))
+            elif piece[0] == 'out' and checkAbba(piece[1]):
+                supports = False
+                break
+        if supports:
+            ans += 1
+            supports = False
 
     print(f'Part One: {ans}')
 
@@ -37,4 +48,3 @@ def partTwo():
 
 partOne()
 partTwo()
-
