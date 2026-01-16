d2e8b4c4c5732b46806662e7d3b5da8ba5bbe2ee
henryChinask1 <jeffbaier72@yahoo.com>
2026-01-16 16:24:34 -0500

    2018day4 part one complete

--- 2018/2018day4.py
+++ 2018/2018day4.py
@@ -1,31 +1,39 @@
-import re
+from datetime import datetime
+from collections import defaultdict, Counter
 
-p = open('Advent of Code Inputs/2018day4TEST.txt').read().split('\n')
+guardstats = []
 
-guardTimes = []
-guardNum = 0
-for i in p:
-    date = re.findall(r'\d{4}-\d{2}-\d{2}', i)
-    time = re.findall(r'\d{2}:\d{2}', i)
-    if re.findall(r'#\d+', i):
-        guardNum = re.findall(r'#(\d+)', i)
-    state = re.findall(r'begins|asleep|wakes', i)
-    guardTimes.append([guardNum[0], date[0], time[0], state[0]])
+with open('Advent of Code Inputs/2018day4.txt') as f:
+    file = f.read().split('\n')
+    for i in file:
+        x = i.split(']')
+        guardStat = []
+        guardStat.append(datetime.strptime(x[0][1:], '%Y-%m-%d %H:%M'))
+        guardStat.append(x[1][1:].split(' '))
+        guardstats.append(guardStat)
 
-def partOne():
-    allGuards = []
-    for i in guardTimes:
-        allGuards.append(i[0])
-    
-    guardStats = {i: [(j, 0) for j in range(0, 76)] for i in set(allGuards)}
-    print(guardStats)
+guardstats.sort()
 
-    for i in guardTimes:
-        if guardStats[i]:
-            guardStats[i[0]][guardStats] = 1
 
-def partTwo():
-    pass
+# {Guard#: minutes asleep, Guard#: minutes asleep}
+sleepTimes = defaultdict(int)
+sleptMins = defaultdict(list)
+guard = ''
+sleepStart = None
+for i in range(len(guardstats)):
+    if guardstats[i][1][0] == 'Guard':
+        guard = guardstats[i][1][1]
+    elif guardstats[i][1][0] == 'falls':
+        sleepStart = guardstats[i][0]
+    elif guardstats[i][1][0] == 'wakes':
+        diff = guardstats[i][0] - sleepStart
+        x = diff.total_seconds()
+        sleepTimes[guard] += int(x // 60)
+        curr = sleepStart.minute
+        while curr < guardstats[i][0].minute:
+            sleptMins[guard].append(curr)
+            curr += 1
 
-partOne()
-partTwo()
+guard = max(sleepTimes, key=sleepTimes.get)
+mostMins = Counter(sleptMins[guard])
+print(f'Part One ans: {int(guard[1:]) * max(mostMins, key=mostMins.get)}')
